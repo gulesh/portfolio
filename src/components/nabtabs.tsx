a7f74e17938f5bcb5d601d7dea860edbd0a21eff
Gulesh <gshukla@haverford.edu>
2024-02-19 21:51:31 -0500

    first commit portfolio website with typescript and nextjs

---
+++ src/components/nabtabs.tsx
@@ -0,0 +1,5 @@
+export default function navtab(text: string){
+	return (
+		//create the navtab here
+		);
+}
---
+++ src/components/nav.tsx
@@ -0,0 +1,12 @@
+function NavBar(){
+	return (
+		<nav>
+			<ul>
+				<li> Home </li>
+				<li> About </li>
+			</ul>
+
+		</nav>
+		);
+}
+export default NavBar;
